32774d53be2b2f8c2691a1774c8d0fad6fe1f4c5
jcsn13 <42977106+jcsn13@users.noreply.github.com>
2018-10-14 15:10:37 -0300

    Update questao_1.py

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -28,9 +28,44 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 1")
-    
+    op = True
+    senhas = ""
+    while op != False:
+	    senha = input("Insira a sua senha: ")
+	    for senha in senha.split(","):
+		    confirm = 0
+		
+		    if len(senha)>=6 and len(senha)<= 12:
+			    confirm+=1
 
+		    for letra in senha:
+			    if letra == '@' or letra == '#' or letra == '$':
+				    confirm+=1
 
+		    for numero in senha:
+			    if numero.isdigit() == True:
+				    confirm+=1
+				    break
+		
+		    for letra in senha:
+			    if letra.isalpha() == True and letra.islower() == True:
+				    confirm+=1
+				    break
+
+		    for letra in senha:
+			    if letra.isalpha() == True and letra.isupper() == True:
+				    confirm+=1
+				    break
+
+		    if confirm == 5:
+			    senhas+= str(senha)+','
+	    print(senhas[:-1])		
+	    op = input("Deseja continuar?(s/n)").lower()
+
+	    if op == 's':
+		    op == True
+	    else:
+		    break
+            
 if __name__ == '__main__':
     main()
