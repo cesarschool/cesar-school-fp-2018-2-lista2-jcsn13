3b7d9b8bc79fe78a911dd51d627ecd4f8a19a06b
jcsn13 <42977106+jcsn13@users.noreply.github.com>
2018-10-15 17:08:26 -0300

    Update questao_2.py

--- questoes/questao_2.py
+++ questoes/questao_2.py
@@ -8,20 +8,20 @@ def main():
 		dir = input("Insira uma direção e quantidade de passos: ").lower()
 		divisor = dir.split(" ") 
 		
-		if dir[:4]=="cima":
+		if dir[:4]=="cima" and dir[4]== " ":
 			print("Sua distancia foi adicionada")
 			contadorV+=int(divisor[1])
 
-		elif dir[:5]=="baixo":
+		elif dir[:5]=="baixo" and dir[5]== " ":
 			print("Sua distancia foi adicionada")
 			contadorV-=int(divisor[1])
 
 
-		elif dir[:8]=="esquerda":
+		elif dir[:8]=="esquerda" and dir[8]== " ":
 			print("Sua distancia foi adicionada")
 			contadorH+=int(divisor[1])
 
-		elif dir[:7]=="direita":
+		elif dir[:7]=="direita" and dir[7]== " ":
 			print("Sua distancia foi adicionada")
 			contadorH-=int(divisor[1])
 		
