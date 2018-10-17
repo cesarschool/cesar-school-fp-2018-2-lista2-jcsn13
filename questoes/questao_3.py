fab733796b6dd33af93b214bb845ed6a723ba9c0
jcsn13 <42977106+jcsn13@users.noreply.github.com>
2018-10-17 20:08:25 -0300

    Update questao_3.py

--- questoes/questao_3.py
+++ questoes/questao_3.py
@@ -45,9 +45,57 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 3")
+	entrada = input("Insira sua ROT e a mensagem que deseja passar: ")
+	alfabeto = "abcdefghijklmnopqrstuvwxyz"
+	alfabetoM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	ROT = 0
+	numeros = ""
+	mensagemN = ""
+	mensagemE = ""
+
+	for u in entrada[5:]:
+		if u.isdigit() == True:
+			entrada = ""
+
+	if entrada[0:3] != "ROT" or entrada[3].isdigit() == False:
+		print("Erro")
+
+	else:
+		for num in entrada:
+			if num.isdigit() == True:
+				numeros+=num
+		
+		ROT = int(numeros)
+		
+		for men in entrada[3:]:
+			if men.isalpha() == True:
+				mensagemN+= men
+			if men == " ":
+				mensagemN+= men
+				
+		mensagemN = mensagemN.lstrip()
+		
+		for c in mensagemN:
+			if c == " ":
+				mensagemE+= c
+			if c.isupper() == True:
+				cin = alfabetoM.index(c)
+				n = cin + ROT
+				while n >= 26:
+					n-= 26
+				mensagemE+= alfabetoM[n]
+				
+			if c.islower() == True:
+				cin = alfabeto.index(c)
+				n = cin + ROT
+				while n >= 26:
+					n-= 26
+				mensagemE+= alfabeto[n]
 
 
-    
+		if entrada[-1] == ".":
+			mensagemE+= entrada[-1]
+
+		print(mensagemE)
 if __name__ == '__main__':
     main()
